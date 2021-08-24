43cb514360f35eb8534a76de1f0284a8481700cf
doppelplusungut <65139362+doppelplusungut@users.noreply.github.com>
2021-08-24 22:12:04 +0200

    update rollierender schnitt

--- app.R
+++ app.R
@@ -8,26 +8,108 @@
 #
 
 library(shiny)
+library(rvest)
+library(ggplot2)
+library(reshape2)
+library(dplyr)
+library(tidyverse)
+library(slider)
+library(tidyquant)
+
+smoothed <- function(electiondata, d) {
+    for(party in c("Union", "SPD", "LINKE", "Gruene", "AfD", "FDP")) {
+        vec = sapply(electiondata$time, FUN = function(x) {
+            mean(electiondata[electiondata$time <= x & electiondata$time >= (x - d),][[party]])
+        })
+        electiondata[[paste("smoothed_",party,sep="")]] <- vec
+    }
+    return(electiondata)
+}
+
+htmlToDf <- function(html, inst) {
+    
+    #this function is a mess and depends highly on the specific formatting on the source site wahlrecht.de. Goes to shit should they ever update
+    
+    df <- html %>%
+        html_element("body") %>%
+        html_elements("table") %>%
+        html_table(header = "TRUE")
+    
+    i = 0
+    
+    time <- df[[2]][[1]] %>% 
+        as.character() %>%
+        as.Date(format = "%d.%m.%Y")
+    
+    i <- which.min(is.na(time))
+    i <- i-1
+    
+    time <- time %>%
+        tail(-i)
+    
+    Union <- df[[2]][["CDU/CSU"]] %>% 
+        tail(-i) %>% 
+        sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+    SPD <- df[[2]][["SPD"]] %>% 
+        tail(-i) %>% 
+        sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+    Gruene <- df[[2]][["GRÜNE"]] %>% 
+        tail(-i) %>% 
+        sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+    FDP <- df[[2]][["FDP"]] %>% 
+        tail(-i) %>% 
+        sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+    LINKE <- df[[2]][["LINKE"]] %>% 
+        tail(-i) %>% 
+        sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+    AfD <- df[[2]][["AfD"]] %>% 
+        tail(-i) %>% 
+        sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+    Sonst <- df[[2]][["Sonstige"]] %>% 
+        tail(-i) %>% 
+        sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+    institute = rep(inst,length(time))
+    
+    return(data.frame(time, Union, SPD, Gruene, FDP, LINKE, AfD, Sonst, institute))
+}
+
+exporteldata <- function() {
+    institutes = data.frame(row.names = c("allensbach", "emnid", "forsa", "politbarometer", "gms", "dimap", "insa", "yougov"))
+    
+    for(name in row.names(institutes)) {
+        institutes[name, "url"] = paste("https://www.wahlrecht.de/umfragen/", name, ".htm", sep = "")
+    }
+    
+    electiondata <- data.frame(time = as.Date(character()), Union = numeric(), SPD = numeric(), Gruene = numeric(), FDP = numeric(), LINKE = numeric(), AfD = numeric(), Sonst = numeric(), institute = character())
+    
+    for(name in row.names(institutes)) {
+        h = read_html(institutes[name,"url"])
+        instdata = htmlToDf(h,name)
+        electiondata = rbind(electiondata, instdata)
+    }
+    
+    electiondata[is.na(electiondata)] <- 0
+    return(electiondata)
+}
+
+electiondata <- exporteldata()
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Sonntagsfrage: Wenn nächsten Sonntag Bundestagswahl wäre..."),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            dateRangeInput("dates", label = h3("Umfragen von ... bis ..."), start = "2021-01-01", format = "dd.mm.yyyy"),
+            sliderInput("smooth_days", label = h3("Anzahl Tage für rollierenden Schnitt"), min = 1, max = 30, value = 7, step = 1)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("feverCurve")
         )
     )
 )
@@ -35,13 +117,22 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
+    output$feverCurve <- renderPlot({
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        
+        d = input$smooth_days
+        
+        data = smoothed(electiondata, d)
+        
+        data = data[data$time >= input$dates[1] & electiondata$time <= input$dates[2],]
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        ggplot(data = data, aes(x = time)) + 
+            geom_line(aes(y = smoothed_Union), color = "black") + 
+            geom_line(aes(y = smoothed_SPD), color = "red") +
+            geom_line(aes(y = smoothed_Gruene), color = "green") +
+            geom_line(aes(y = smoothed_AfD), color = "steelblue") + 
+            geom_line(aes(y = smoothed_FDP), color = "yellow") + 
+            geom_line(aes(y = smoothed_LINKE), color = "pink")
     })
 }
 
--- parse.R
+++ parse.R
@@ -1,6 +1,25 @@
-library(rvest)
+pacman::p_load(
+  shiny,
+  rvest,
+  ggplot2,
+  reshape2,
+  dplyr,
+  tidyverse,      # for data management and viz
+  slider,         # for calculating moving averages
+  tidyquant       # for calculating moving averages within ggplot
+)
 
-htmlToDf <- function(html) {
+smoothed <- function(electiondata, d) {
+  for(party in c("Union", "SPD", "LINKE", "Gruene", "AfD", "FDP")) {
+    vec = sapply(electiondata$time, FUN = function(x) {
+      mean(electiondata[electiondata$time <= x & electiondata$time >= (x - d),][[party]])
+    })
+    electiondata[[paste("smoothed_",party)]] <- vec
+  }
+  return(electiondata)
+}
+
+htmlToDf <- function(html, inst) {
   
 #this function is a mess and depends highly on the specific formatting on the source site wahlrecht.de. Goes to shit should they ever update
 
@@ -9,25 +28,59 @@ df <- html %>%
   html_elements("table") %>%
   html_table(header = "TRUE")
 
-time <- df[[2]][[1]] %>% tail(-3) %>% as.Date(format = "%d.%m.%Y")
-Union <- df[[2]][["CDU/CSU"]] %>% tail(-3) %>% sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
-SPD <- df[[2]][["SPD"]] %>% tail(-3) %>% sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
-Gruene <- df[[2]][["GRÜNE"]] %>% tail(-3) %>% sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
-FDP <- df[[2]][["FDP"]] %>% tail(-3) %>% sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
-LINKE <- df[[2]][["LINKE"]] %>% tail(-3) %>% sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
-AfD <- df[[2]][["AfD"]] %>% tail(-3) %>% sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
-Sonst <- df[[2]][["Sonstige"]] %>% tail(-3) %>% sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+i = 0
 
-return(data.frame(time, Union, SPD, Gruene, FDP, LINKE, AfD, Sonst))
+time <- df[[2]][[1]] %>% 
+  as.character() %>%
+  as.Date(format = "%d.%m.%Y")
+
+i <- which.min(is.na(time))
+i <- i-1
+
+time <- time %>%
+  tail(-i)
+  
+Union <- df[[2]][["CDU/CSU"]] %>% 
+  tail(-i) %>% 
+  sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+SPD <- df[[2]][["SPD"]] %>% 
+  tail(-i) %>% 
+  sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+Gruene <- df[[2]][["GRÜNE"]] %>% 
+  tail(-i) %>% 
+  sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+FDP <- df[[2]][["FDP"]] %>% 
+  tail(-i) %>% 
+  sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+LINKE <- df[[2]][["LINKE"]] %>% 
+  tail(-i) %>% 
+  sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+AfD <- df[[2]][["AfD"]] %>% 
+  tail(-i) %>% 
+  sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+Sonst <- df[[2]][["Sonstige"]] %>% 
+  tail(-i) %>% 
+  sapply(X = ., FUN = function(x) {(as.numeric(gsub(" %", "", gsub(",",".", x))))/100})
+institute = rep(inst,length(time))
+
+return(data.frame(time, Union, SPD, Gruene, FDP, LINKE, AfD, Sonst, institute))
 }
 
-institutes = data.frame(row.names = c("allensbach", "emnid", "forsa", "politbarometer", "gms", "dimap", "insa", "yougov"))
-
-for(name in row.names(institutes)) {
-  institutes[name, "url"] = paste("https://www.wahlrecht.de/umfragen/", name, ".htm", sep = "")
-}
-
-for(name in row.names(institutes)) {
-  institutes[name, "data"] = read_html(institutes[name, "url"]) %>% 
-    htmlToDf
+exporteldata <- function() {
+  institutes = data.frame(row.names = c("allensbach", "emnid", "forsa", "politbarometer", "gms", "dimap", "insa", "yougov"))
+  
+  for(name in row.names(institutes)) {
+    institutes[name, "url"] = paste("https://www.wahlrecht.de/umfragen/", name, ".htm", sep = "")
+  }
+  
+  electiondata <- data.frame(time = as.Date(character()), Union = numeric(), SPD = numeric(), Gruene = numeric(), FDP = numeric(), LINKE = numeric(), AfD = numeric(), Sonst = numeric(), institute = character())
+  
+  for(name in row.names(institutes)) {
+    h = read_html(institutes[name,"url"])
+    instdata = htmlToDf(h,name)
+    electiondata = rbind(electiondata, instdata)
+  }
+  
+  electiondata[is.na(electiondata)] <- 0
+  return(electiondata)
 }
