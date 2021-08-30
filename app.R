78ef9fd9eff0fa29fcad65298aa582c3a27dc196
doppelplusungut <65139362+doppelplusungut@users.noreply.github.com>
2021-08-30 12:29:57 +0200

    Added tabs, some refactoring

--- app.R
+++ app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(rvest)
 library(ggplot2)
@@ -20,6 +11,15 @@ library(DT)
 
 ndayaverage <- function(electiondata, n, date, party) {
     return(mean(electiondata[electiondata$time <= date & electiondata$time >= (date - n),][[party]]))
+}
+
+normalized_latest_surveys <- function(electiondata, n) {
+    surveydata = latest_surveys(electiondata, n)
+    normalized_value = sapply(surveydata$value, FUN = function(x) {
+        x / sum(surveydata$value)
+    })
+    surveydata$value = normalized_value
+    return(surveydata)
 }
 
 latest_surveys <- function(electiondata, n) {
@@ -111,34 +111,38 @@ exporteldata <- function() {
 
 electiondata <- exporteldata()
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
     titlePanel("Sonntagsfrage: Wenn nächsten Sonntag Bundestagswahl wäre..."),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            dateRangeInput("dates", label = h3("Umfragen von ... bis ..."), start = "2021-01-01", format = "dd.mm.yyyy"),
-            sliderInput("smooth_days", label = h3("Anzahl Tage für rollierenden Schnitt"), min = 1, max = 30, value = 7, step = 1)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("feverCurve"),
-           plotOutput("partyPie"),
-           h2("Party test"),
-           dataTableOutput("partytable")
+    mainPanel(
+        tabsetPanel(
+            tabPanel("Zeitentwicklung",
+                    sidebarPanel(
+                        dateRangeInput("dates", label = h3("Umfragen von ... bis ..."), start = "2021-01-01", format = "dd.mm.yyyy"),
+                        sliderInput("smooth_days", label = h3("Anzahl Tage für rollierenden Schnitt"), min = 1, max = 30, value = 7, step = 1)
+                    ),
+                    mainPanel(
+                        plotOutput("feverCurve")
+                    )
+            ),
+            tabPanel("Aktuell",
+                     sidebarPanel(
+                         sliderInput("last_days", label = h3("Umfragen der letzten n Tage mit n = "), min = 1, max = 30, value = 7, step = 1)
+                     ),
+                     mainPanel(
+                         DT::dataTableOutput("partytable"),
+                         plotOutput("partyPie")
+                     )
+            )
         )
     )
+
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
 
     output$feverCurve <- renderPlot({
-        # generate bins based on input$bins from ui.R
         
         d = input$smooth_days
         
@@ -160,15 +164,15 @@ server <- function(input, output) {
     })
     
     output$partyPie <- renderPlot({
-        ggplot(data = latest_surveys(electiondata, input$smooth_days), aes(x = "", y = value, fill = parties)) +
+        ggplot(data = normalized_latest_surveys(electiondata, input$last_days), aes(x = "", y = value, fill = parties)) +
             geom_bar(stat = "identity", width = 1) + 
             coord_polar("y", start = 0)
     })
     
-    output$partytable <- renderDataTable({
-        latest_surveys(electiondata, input$smooth_days)
+    output$partytable <- DT::renderDataTable({
+        DT::datatable((normalized_latest_surveys(electiondata, input$last_days) %>%
+            mutate_if(is.numeric, round, digits = 3)), options = list(paging = FALSE, searching = FALSE, info = FALSE, order = list(2, 'desc')))
     })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
